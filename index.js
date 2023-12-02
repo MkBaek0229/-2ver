623e85a52a52b99edcbf8be7fefad0abbeaf0b4a
mac <mac@macui-MacBookAir.local>
2023-12-02 21:07:43 +0900

    dom으로 nav 메뉴목록들불러오기

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.html의 네비게이션 메뉴에서 각 항목을 클릭했을때 해당 페이지로 이동하도록 이벤트 처리를 구현
+// dom 조작 addEventListener() 메서드를 사용해 클릭 이벤트를 감지 클릭된 메뉴애 따라 해당페이지를 보여주도록한다.
+
+// 일단 메뉴 목록들을 따로 dom으로 불러오자
+const $Home = document.getElementById("Home")
+
+const $Record = document.getElementById("Work_record")
+
+const $Statistics = document.getElementById("Statistics")
+
+const $Setting = document.getElementById("Setting")
+
